7ed73ce8e6a8bc70a6560c4769e7e35e1a45178d
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-09-06 11:10:19 +0600

    indivisual id done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 // Create an instance of the Express application
 const app = express();
+const cors =require("cors")
 const port = 3000; // Define the port number
 
 // Define a simple route
@@ -11,9 +12,18 @@ app.get('/', (req, res) => {
 });
 
 const categories=require('./data/categories.json')
+
+app.use(cors())
 //
 app.get('/categories', (req, res) => {
     res.send(categories);
+  });
+
+
+  app.get('/categories/:id', (req, res) => {
+    const id=req.params.id;
+    const selectedRecepi= categories.find(n=>parseInt(n._id)===id)
+    res.send(selectedRecepi)
   });
 
 // Start the server and listen on the defined port
